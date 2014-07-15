85a9c32fdb986d66b81806ffa9474bdbc4467f98
evilBird <travishenspeter@Traviss-MacBook-Pro-2.local>
2014-07-14 21:11:42 -0500

    Added BSDAverage

---
+++ BSDPatchExample/BSDAverage.h
@@ -0,0 +1,13 @@
+//
+//  BSDAverage.h
+//  BSDPatchExample
+//
+//  Created by Travis Henspeter on 7/14/14.
+//  Copyright (c) 2014 birdSound LLC. All rights reserved.
+//
+
+#import "BSDObject.h"
+
+@interface BSDAverage : BSDObject
+
+@end
---
+++ BSDPatchExample/BSDAverage.m
@@ -0,0 +1,13 @@
+//
+//  BSDAverage.m
+//  BSDPatchExample
+//
+//  Created by Travis Henspeter on 7/14/14.
+//  Copyright (c) 2014 birdSound LLC. All rights reserved.
+//
+
+#import "BSDAverage.h"
+
+@implementation BSDAverage
+
+@end
